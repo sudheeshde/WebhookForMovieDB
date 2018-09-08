4b20dd849c12a648fb3034f366da8435bc56a5b3
Sudheesh R <sudheeshde@gmail.com>
2018-09-08 20:21:54 +0530

    added actions checking for imdb rating

--- index.js
+++ index.js
@@ -11,15 +11,17 @@ server.use(bodyParser.urlencoded({
 }));
 
 server.use(bodyParser.json());
+
 server.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
     next();
 });
+
 server.post('/get-movie-details', (req, res) => {
 
-      const movieToSearch = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.movie ? req.body.queryResult.parameters.movie : 'The Godfather';
+    const movieToSearch = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.movie ? req.body.queryResult.parameters.movie : 'The Godfather';
     const reqUrl = encodeURI(`http://www.omdbapi.com/?t=${movieToSearch}&apikey=${API_KEY}`);
     http.get(reqUrl, (responseFromAPI) => {
         let completeResponse = '';
@@ -29,7 +31,7 @@ server.post('/get-movie-details', (req, res) => {
         responseFromAPI.on('end', () => {
             const movie = JSON.parse(completeResponse);
             let dataToSend = movieToSearch === 'The Godfather' ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.\n` : '';
-            dataToSend += `${movie.Title} is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}`;
+            dataToSend += `${movie.Title} is a ${movie.Actors} starrer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}`;
 
             return res.json({
                 fulfillmentText: dataToSend,
@@ -44,32 +46,39 @@ server.post('/get-movie-details', (req, res) => {
     });
 });
 
-server.post('/get-imdb-rating', (req, res) => {
+let action = req.body.result.action;
 
-    const movieToSearch = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.movie ? req.body.queryResult.parameters.movie : 'The Godfather';
-    const reqUrl = encodeURI(`http://www.omdbapi.com/?t=${movieToSearch}&apikey=${API_KEY}`);
-    http.get(reqUrl, (responseFromAPI) => {
-        let completeResponse = '';
-        responseFromAPI.on('data', (chunk) => {
-            completeResponse += chunk;
-        });
-        responseFromAPI.on('end', () => {
-            const movie = JSON.parse(completeResponse);
-            let dataToSend = movieToSearch === 'The Godfather' ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.\n` : '';
-            dataToSend += `IMDB rating of ${movie.Title} is ${movie.imdbRating}`;
+if (action === 'get-imdb-rating') {
 
+    server.post('/get-imdb-rating', (req, res) => {
+
+        const movieToSearch = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.movie ? req.body.queryResult.parameters.movie : 'The Godfather';
+        const reqUrl = encodeURI(`http://www.omdbapi.com/?t=${movieToSearch}&apikey=${API_KEY}`);
+        http.get(reqUrl, (responseFromAPI) => {
+            let completeResponse = '';
+            responseFromAPI.on('data', (chunk) => {
+                completeResponse += chunk;
+            });
+            responseFromAPI.on('end', () => {
+                const movie = JSON.parse(completeResponse);
+                let dataToSend = movieToSearch === 'The Godfather' ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.\n` : '';
+                dataToSend += `IMDB rating of ${movie.Title} is ${movie.imdbRating}`;
+
+                return res.json({
+                    fulfillmentText: dataToSend,
+                    source: 'get-imdb-rating'
+                });
+            });
+        }, (error) => {
             return res.json({
-                fulfillmentText: dataToSend,
+                fulfillmentText: 'Something went wrong!',
                 source: 'get-imdb-rating'
             });
         });
-    }, (error) => {
-        return res.json({
-            fulfillmentText: 'Something went wrong!',
-            source: 'get-imdb-rating'
-        });
     });
-});
+
+}
+
 
 server.listen((process.env.PORT || 8000), () => {
     console.log("Server is up and running...");
