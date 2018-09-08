d334d78f4aa5860b71fc9a67d023df0775967613
Sudheesh R <sudheeshde@gmail.com>
2018-09-08 18:14:27 +0530

    added get-imdb-rating code

--- index.js
+++ index.js
@@ -44,6 +44,33 @@ server.post('/get-movie-details', (req, res) => {
     });
 });
 
+server.post('/get-imdb-rating', (req, res) => {
+
+    const movieToSearch = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.movie ? req.body.queryResult.parameters.movie : 'The Godfather';
+    const reqUrl = encodeURI(`http://www.omdbapi.com/?t=${movieToSearch}&apikey=${API_KEY}`);
+    http.get(reqUrl, (responseFromAPI) => {
+        let completeResponse = '';
+        responseFromAPI.on('data', (chunk) => {
+            completeResponse += chunk;
+        });
+        responseFromAPI.on('end', () => {
+            const movie = JSON.parse(completeResponse);
+            let dataToSend = movieToSearch === 'The Godfather' ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.\n` : '';
+            dataToSend += `IMDB rating of ${movie.Title} is ${movie.imdbRating}`;
+
+            return res.json({
+                fulfillmentText: dataToSend,
+                source: 'get-imdb-rating'
+            });
+        });
+    }, (error) => {
+        return res.json({
+            fulfillmentText: 'Something went wrong!',
+            source: 'get-imdb-rating'
+        });
+    });
+});
+
 server.listen((process.env.PORT || 8000), () => {
     console.log("Server is up and running...");
 });
